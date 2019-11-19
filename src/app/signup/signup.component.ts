c33848695e6739ead92634c6471ed184dab9f92a
kiranraj2208 <kiranraj2208@gmail.com>
2019-11-19 07:52:11 +0530

    signup clear

--- src/app/signup/signup.component.ts
+++ src/app/signup/signup.component.ts
@@ -1,71 +1,98 @@
-import { Component, OnInit } from '@angular/core';
-import {FormControl, FormGroupDirective, NgForm, Validators, AbstractControl, ValidatorFn, FormGroup} from '@angular/forms';
-import {ErrorStateMatcher} from '@angular/material/core';
-import {UserLoginServiceService} from '../user-login-service.service';
+import { Component, OnInit, Input } from "@angular/core";
+import {
+  FormControl,
+  FormGroupDirective,
+  NgForm,
+  Validators,
+  AbstractControl,
+  ValidatorFn
+} from "@angular/forms";
+import { ErrorStateMatcher } from "@angular/material/core";
+import { UserLoginServiceService } from "../user-login-service.service";
 
 /** Error when invalid control is dirty, touched, or submitted. */
 export class MyErrorStateMatcher implements ErrorStateMatcher {
-  isErrorState(control: FormControl | null, form: FormGroupDirective | NgForm | null): boolean {
+  isErrorState(
+    control: FormControl | null,
+    form: FormGroupDirective | NgForm | null
+  ): boolean {
     const isSubmitted = form && form.submitted;
-    return !!(control && control.invalid && (control.dirty || control.touched || isSubmitted));
+    return !!(
+      control &&
+      control.invalid &&
+      (control.dirty || control.touched || isSubmitted)
+    );
   }
 }
 
 @Component({
-  selector: 'app-signup',
-  templateUrl: './signup.component.html',
-  styleUrls: ['./signup.component.css']
+  selector: "app-signup",
+  templateUrl: "./signup.component.html",
+  styleUrls: ["./signup.component.css"]
 })
 export class SignupComponent implements OnInit {
-
   hide = true;
-  usernameFormControl = new FormControl('', [
+  @Input() signup: boolean;
+  usernameFormControl = new FormControl("", [
     Validators.required,
     Validators.minLength(6),
-    forbiddenNameValidator(/aa/i, this.userLoginService),
-  ])
-  emailFormControl = new FormControl('', [
+    forbiddenNameValidator()
+  ]);
+  emailFormControl = new FormControl("", [
     Validators.required,
-    Validators.email,
+    Validators.email
     // checkEmailExists(/a/i, this.userLoginService),
   ]);
-  passwordFormControl = new FormControl('', [
+  passwordFormControl = new FormControl("", [
     Validators.required,
-    Validators.minLength(8),
+    Validators.minLength(8)
   ]);
-  constructor(private userLoginService:UserLoginServiceService) {}
-
+  constructor(private userLoginService: UserLoginServiceService) {}
 
   matcher = new MyErrorStateMatcher();
 
   onsubmit() {
-   
     this.emailFormControl.setErrors({
-      emailExists: this.userLoginService.checkEmailAlreadyExists(this.emailFormControl.value)
+      emailExists: this.userLoginService.checkEmailAlreadyExists(
+        this.emailFormControl.value
+      )
     });
     this.usernameFormControl.setErrors({
-      usernameExists: this.userLoginService.checkUsernameAlreadyExists(this.usernameFormControl.value)
-    })
-    if(!this.emailFormControl.hasError('emailExists') && !this.usernameFormControl.hasError('usernameExists'))
-    this.userLoginService.registerUser(this.usernameFormControl.value ,this.emailFormControl.value, this.passwordFormControl.value);
+      usernameExists: this.userLoginService.checkUsernameAlreadyExists(
+        this.usernameFormControl.value
+      )
+    });
+    if (
+      !this.emailFormControl.hasError("emailExists") &&
+      !this.usernameFormControl.hasError("usernameExists")
+    )
+      this.userLoginService.registerUser(
+        this.usernameFormControl.value,
+        this.emailFormControl.value,
+        this.passwordFormControl.value
+      );
   }
 
-  ngOnInit() {
+  ngOnInit() {}
+  ngOnChanges(changes: any) {
+    if(!this.signup){
+      this.emailFormControl.reset();
+      this.usernameFormControl.reset();
+      this.passwordFormControl.reset();
+      this.signup = true;
+    }
   }
-
 }
 
-export function forbiddenNameValidator(nameRe: RegExp, userLoginService:UserLoginServiceService): ValidatorFn {
-  return (control: AbstractControl): {[key: string]: any} | null => {
+export function forbiddenNameValidator(): ValidatorFn {
+  return (control: AbstractControl): { [key: string]: any } | null => {
     const username = /^[ \w\d]+$/;
     // const email = /^[\w\d\._%+-]+@[\w\d\.-]+\.\w{2,}/;
-    let lower = false;
-    const validUsername  = username.test(control.value);
-    if(control.value === control.value.toLowerCase())
-      lower = true;
-     if((!validUsername || !lower) && control.value != '') return {'forbiddenName': {value: control.value}} 
-     else
-     return null;
-
-  }
+    let lower = true;
+    const validUsername = username.test(control.value);
+    if (control.value && control.value !== control.value.toLowerCase()) lower = false;
+    if ((!validUsername || !lower) && control.value != "")
+      return { forbiddenName: { value: control.value } };
+    else return null;
+  };
 }
